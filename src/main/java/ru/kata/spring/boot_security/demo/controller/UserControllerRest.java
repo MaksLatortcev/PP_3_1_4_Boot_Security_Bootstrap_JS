bfebcce1c6cab96af7c4b9b496697240d643d1c9
MaksLatortcev <maksim_anatolievish@mail.ru>
2022-05-29 00:12:30 +0300

    Done

--- src/main/java/ru/kata/spring/boot_security/demo/controller/UserControllerRest.java
+++ src/main/java/ru/kata/spring/boot_security/demo/controller/UserControllerRest.java
@@ -17,6 +17,7 @@ import java.util.List;
 public class UserControllerRest {
 
     private UserService userService;
+
     private RoleService roleService;
 
     private PasswordEncoder passwordEncoder;
